8c6aac03fc4e76c851c832357f45930c6ac0ccb1
chaoukat99 <chawkatomar@gmail.com>
2025-06-17 21:29:43 +0100

    Learn Js

---
+++ LearnJs/app.js
@@ -0,0 +1,242 @@
+// console.log("hello guys")
+
+// let user="sara"
+
+// user="Karim"
+// let age=30
+
+
+// let prénom="Omar"
+
+
+
+
+
+// console.log(user)
+// console.log(age)
+
+// console.log(prénom)
+
+
+
+
+// console.log("*".repeat(1000))
+
+
+
+// console.log("Bonjour "+user)
+
+
+
+
+
+
+// DRY(don't repeat yourself)
+
+
+// let username="Mohammed" ;
+
+// username="Malek";
+
+
+// console.log("Bonjour "+username);
+
+
+
+
+
+// console.log("Bonsoir "+username);
+
+
+
+
+
+// console.log("Bon weekend "+username)
+
+
+
+
+// const adminEmail="admin@gmail.com";
+// let school="UIR";
+
+// console.log(school);
+
+
+
+
+// variable syntax : let nom_de_la_variable = valeur;
+
+
+
+// let USER="Samir";
+
+
+
+// let age=20;
+
+
+
+// let user ="samir";
+
+
+
+// constante// const nom_de_la_constante = valeur;
+
+
+
+// Type de données en Js 
+
+
+
+
+// let usernmae='Samir';  // String " " ' '
+
+
+
+// let Email="samir@gmail.com";
+
+
+
+
+// let city = "Casablanca"; 
+
+// let city2="Rabat";
+
+// let ville =city2 // 5
+
+// console.log(ville);
+
+// console.log(city.length)
+
+// lenght c'est une propriété qui retourne la taille d'une chaîne de caractères(String)
+
+
+
+
+// let user1="Imad";
+
+// let user2="Youssef";
+
+
+
+// let user3=user2; //   Youssef
+
+// user3=user1;  // Imad
+
+// console.log(user3);
+
+
+
+
+// Number 
+
+let age =30; // Number
+
+
+let salary=20000.23; // Number 
+
+
+
+
+// let year=20;
+
+
+// console.log(year)
+
+
+
+// let client=null;
+
+
+// Boolean 
+
+
+// let userIsConnected=false;
+
+
+
+
+
+// let schoolName="Groupe scolaire Gomycode"; // String
+
+
+// let courseCount=20;  // NUmber
+
+
+// let MoyenneDeGroupe=15.7; // Number
+
+
+
+// let EcolePrivée=false; // Boolean 
+
+
+
+// let Trophée=null;
+
+
+
+
+
+// Opérateurs
+
+// -Arithmétique
+// +  -  *  /  %  **
+
+
+let number1=10;
+
+let number2=2;
+
+
+console.log(number1**number2);
+
+
+
+const TVA=20;
+
+
+
+let produitHT=2000;
+
+
+let prouits=produitHT+(produitHT*TVA/100)
+
+
+
+
+// Déclarer deux variable de type Numeber et afficher la somme, la différence, le produit et le quotient de ces deux variables.
+
+
+
+
+let nb1=50;
+let nb2=0;
+
+
+// console.log(nb1+nb2);
+
+// console.log(nb1-nb2);
+
+// console.log(nb1*nb2);
+
+
+// console.log(nb1/nb2);
+
+
+// console.log(nb1%nb2);
+
+
+// Opérateur de comparaison
+let num1="20";
+let num2=20;
+
+// < > <= >= == === != 
+
+
+
+// console.log(num1!=num2)
+
+
+console.log("2000"===2000);
+
+
