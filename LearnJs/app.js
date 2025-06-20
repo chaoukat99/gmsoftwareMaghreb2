7a45d8de89d380b8679e9584b2b131727ab11c62
chaoukat99 <chawkatomar@gmail.com>
2025-06-20 22:17:34 +0100

    20/06/2025

--- LearnJs/app.js
+++ LearnJs/app.js
@@ -1,5 +1,7 @@
 // console.log("hello guys")
 
+
+
 // let user="sara"
 
 // user="Karim"
@@ -130,7 +132,7 @@
 
 // Number 
 
-let age =30; // Number
+let age =10; // Number
 
 
 let salary=20000.23; // Number 
@@ -188,7 +190,7 @@ let number1=10;
 let number2=2;
 
 
-console.log(number1**number2);
+// console.log(number1**number2);
 
 
 
@@ -227,8 +229,7 @@ let nb2=0;
 
 
 // Opérateur de comparaison
-let num1="20";
-let num2=20;
+
 
 // < > <= >= == === != 
 
@@ -237,6 +238,428 @@ let num2=20;
 // console.log(num1!=num2)
 
 
-console.log("2000"===2000);
+// console.log("2000"===2000);
 
 
+
+
+// Les Opérateurs logiques
+  // && || !  
+  let num1="20";
+  let num2=20;
+  
+  
+let email="Samir@gmail.com";
+
+let password="Samir123";
+
+let inputEmail="Samir@gmail.com";
+let inputPassword="Samir123";
+
+// console.log(email==inputEmail && password==inputPassword && num1===num2);   // true  && true  && false=> false
+
+// true && true => true
+// false && true => false
+// true && false => false   
+// false && false => false
+
+
+
+
+// console.log("3"===3);
+
+
+
+let numDIdentité="MA1223";
+
+
+
+let passportId="MA1242";
+
+
+// console.log(numDIdentité=="MA11223" || passportId=="MA1212"); 
+
+            // true  || true  =>true 
+
+            // true || false => true 
+
+            // false || false => false
+
+
+
+
+            
+let a = 10 ;
+let b = a+5;
+
+let c=b-5*2;
+
+let d=c+5*2
+
+
+
+
+// let test3=  b%a!=0 || "25"===(c**2) && d+5>=a*2;
+            // true || false=false
+
+// let test = a>b || "hello".length!=c && d>=c;
+
+
+// false ||  false && true   =false 
+
+// console.log(test); // false
+
+// console.log(b%a!=0 ); // false
+
+
+
+// let test5=false;   // ! (Not)
+
+// let test7=!(30==="30");
+
+// console.log(test7); // true
+
+
+
+
+
+// Control Flow 
+
+
+// Les conditions 
+
+
+
+const userAge=10;
+
+// if(userAge>=18){
+//     console.log("Vous êtes majeur");
+// }
+
+// if(userAge<18){
+//     console.log("Vous êtes mineur");
+// }
+
+
+// if(condition){
+//    // instructions à exécuter si la condition est vraie 
+// }
+
+
+
+
+// let username="samir9";
+// let userPassword="Samir123";
+
+
+
+// if(username=="samir99" && userPassword=="Samir123"){
+
+//     console.log("utilisateur Trouvé avec succès");
+// }
+
+
+
+
+let day=10;
+
+
+// des conditions basé sur la variable day pour afficher le jour de la semaine correspondant
+// 1 => Lundi 
+// 2 => Mardi
+
+
+// if(day<1 || day >7){
+//     console.log("Jour invalide");
+// }
+
+// if(day == 1){
+//     console.log("Lundi");
+    
+// }
+
+// if(day == 2){
+//     console.log("Mardi");
+    
+// }
+// if(day == 3){
+//     console.log("Mercredi");
+    
+// }
+// if(day == 4){
+//     console.log("Jeudi");
+    
+// }
+// if(day == 5){
+//     console.log("Vendredi");
+    
+// }
+// if(day == 6){
+//     console.log("Samedi");
+    
+// }
+// if(day == 7){
+//     console.log("Dimanche");
+    
+// }
+
+
+
+
+let userage=15;
+
+
+// if(userage>=18){
+//     console.log("Vous êtes majeur");
+// }else{
+//     console.log("Vous êtes mineur");
+// }
+
+
+
+
+//  nested if else 
+
+
+day=3+10;
+
+
+// if(day==1){
+//     console.log("Lundi");
+// }else if(day==2){
+//     console.log("Mardi");
+// }else if(day==3){
+//     console.log("Mercredi");
+// }else if(day==4){
+//     console.log("Jeudi");
+// }else if(day==5){
+//     console.log("Vendredi");
+// }else if(day==6){
+//     console.log("Samedi");
+// }else if(day==7){
+//     console.log("Dimanche");
+// }else{
+//     console.log("Jour invalide");
+// }
+
+
+
+// switch case 
+
+
+
+
+// let prénom="hakim";
+
+
+// switch(prénom){
+//     case "Omar":
+//         console.log("Bonjour Omar");
+//         break;
+//     case "Samir":
+//         console.log("Bonjour Samir");
+//         break;
+//     case "Hakim":
+//         console.log("Bonjour Hakim");
+//         break;
+//     case "hakim":
+//         console.log("Bonjour Hakim en minuscule");
+//         break;    
+        
+//     default:{
+//         console.log("Valeur Incorrecte ")
+//     }    
+// }
+
+
+
+
+let result ="";
+
+
+
+let stock=10;
+
+// if(stock>=1){
+//     result="Stock disponible";
+// }else{
+//     result="Stock épuisé";
+// }
+
+
+// Condition Ternaire 
+
+
+result=stock>=1 ? "Stock disponible" : "Stock épuisé";
+
+    //  (condition ? valeur si vrai : valeur si faux)
+
+
+// statuss=age>=18 ? "Majeur":"Mineur"    ;
+
+
+// console.log(result);
+// console.log(statuss);
+
+
+
+
+
+// Alert Prompt Confirm
+
+
+
+// alert("Paiement a été traiter avec succés") // string;
+
+
+// confirm("message") // methode qui retourne  une valeur booléenne true ou false
+
+
+
+// let test=confirm("Voulez-vous continuer ?"); 
+
+
+
+
+
+// console.log(test);
+
+
+// let username=prompt("Veuillez Saisir votre nom d'utilisateur"); //
+
+// let type1=12;
+// let type2="hello";
+// let type3 =true;
+
+// console.log(username);
+
+
+// typeof
+
+// console.log(typeof(type1));
+// console.log(typeof(type2));
+// console.log(typeof(type3));
+
+
+
+
+
+// Demander à utilisateur de saisir son age et vous allez 
+// afficeher le message "Vous êtes majeur" si l'age est supérieur ou égal à 18 sinon afficher "Vous êtes mineur".
+// En utilisant alert , prompt
+
+
+
+
+
+
+//  num1="10"; 
+
+//  num2="1";
+
+
+//  console.log(+num1 + +num2);
+
+
+// let nom="Salimi";
+// let prénom="samir"
+//  console.log("Hello my firstname is "+prénom+" and my lastname is "+nom); // concatenation
+
+
+
+
+//  Exercice : 
+
+// Afficher Un menu sous forme alert qui va afficher le message suivant :
+// 1- Addition | 2- Soustraction | 3- Multiplication | 4- Division | 5- Quitter
+
+
+// il faut que l'utilisateur saisit deux nombre 
+
+// finalement vous devrez afficher le résultat de l'opération choisie par l'utilisateur sous forme d'alert (Le résultat de l'addition est : x)
+
+
+
+
+// alert("1- Addition | 2- Soustraction | 3- Multiplication | 4- Division | 5- Quitter")
+
+
+// let nombre1=prompt("Veuillez saisir le premier nombre 1");
+// let nombre2=prompt("Veuillez saisir le premier nombre 2");
+
+
+// let choix=prompt("Veuillez saisir votre choix 1, 2, 3, 4 ou 5");
+
+// let result2;
+
+// switch(choix){
+   
+//      case "1":{
+//       result2=+nombre1+ +nombre2;
+//      }
+//      break ; 
+
+//      case "2":{
+//       result2=nombre1-nombre2;
+//      }
+//      break;
+//      case "3":{
+//       result2=nombre1*nombre2;
+//      }
+//      break;
+//      case "4":{
+//         if(nombre2==0){
+//           result2="Impossible de diviser par zéro";
+//         }else{
+//           result2=nombre1/nombre2;
+//         }
+//      }
+
+//      break;
+
+//      case "5":{
+//       alert("Merci d'avoir utilisé notre application");
+//      }
+//      break;
+//      default:{
+//        alert("Choix Invalide")
+//      }
+
+// }
+
+
+
+// alert(result2);
+
+
+ email="email@gmail.com";
+ let mobile="+21209080706";
+
+
+
+// let phrase="Mon email est "+email+" et mon numéro de téléphone est "+mobile;
+
+// console.log(phrase)
+
+
+
+
+// let str="Monsieur le Directeur \n     Je vous écris pour vous ... "
+
+// console.log(str);
+
+
+// Exercice:
+
+// nom prénom age email mobile profession 
+
+
+/*
+Name: samiri karim
+Age : 30 
+Email:email@gmail.com
+Mobile: +21209080706
+Profession: Développeur Web
+
+
+*/
