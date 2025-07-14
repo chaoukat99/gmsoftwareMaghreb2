f5b2d8463057f73cdb9a83b84d2f510f7b739c44
chaoukat99 <chawkatomar@gmail.com>
2025-07-14 21:21:54 +0100

    Dom concepts

---
+++ LearnJs/Dom.js
@@ -0,0 +1,62 @@
+// Sélécteurs
+// Html Tag content 
+
+// attributes Manipulation
+
+// css  style with Js 
+
+// classlist Object 
+// Nodes 
+
+// Create Elements with Js 
+
+// Javascript Events
+
+
+
+// DOM (document object Model)
+
+// Sélection Des élements Html  
+
+
+
+// getElementById
+
+
+let titre= document.getElementById("mainTitle");
+
+
+// GetElementsByTagName
+
+
+let paras=document.getElementsByTagName("meta"); // []
+
+
+
+
+// getElementsByClassName
+
+
+// let classes=document.getElementsByClassName("cls"); // []
+
+
+
+// querySelector (elle applique la selection css)
+
+
+let firstElementWithClass=document.querySelector(".cls");
+
+
+// querySelectorAll (elle applique la selection css mais elle collectent tout les élements séléctionné en [] )
+
+
+
+let allClsClasseElement=document.querySelectorAll(".cls"); // []
+
+console.log(titre);
+console.log(paras);
+
+// console.log(classes);
+console.log(firstElementWithClass)
+
+console.log(allClsClasseElement)
