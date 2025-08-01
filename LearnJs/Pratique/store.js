073e3c1d122f8db55fdc94d4b4df0813d008a77f
chaoukat99 <chawkatomar@gmail.com>
2025-08-01 01:20:25 +0100

    new commit 2

--- LearnJs/Pratique/store.js
+++ LearnJs/Pratique/store.js
@@ -135,7 +135,7 @@ Mtbtn.onclick=()=>{
 
    }else{
                Swal.fire({
-                title: `Nothing To Order ` ,
+                title: `Nothing To Orders ` ,
                 text: "Try Later",
                 icon: "error"
                 });
